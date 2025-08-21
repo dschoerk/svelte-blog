b25d5bee3e7952feb09c778e98d3423b36b67128
dschoerk <anon>
2025-08-21 13:54:30 +0200

    md template is working with a workaround

---
+++ src/lib/post-layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let title = "";
+    export let date = "";
+</script>
+
+<article>
+    <header>
+        <h1>{title}</h1>
+        <p>{date}</p>
+    </header>
+    asdf
+    
+
+    <slot />
+
+</article>
--- src/posts/hello-world.md
+++ src/posts/hello-world.md
@@ -1,4 +1,7 @@
-
+---
+title: "Hello World"
+date: "2024-01-15"
+---
 
 # Hello World
 This is my first post
